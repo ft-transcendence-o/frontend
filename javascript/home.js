7f24db39ac29e526de31b2cf2235af92086f25ae
junssong <xms2007@naver.com>
2024-07-19 17:22:37 +0900

    feat: access toeken

--- javascript/home.js
+++ javascript/home.js
@@ -1,8 +1,20 @@
+function decodeJWTManually(token) {
+    const parts = token.split('.');
+    if (parts.length !== 3) {
+      throw new Error('Invalid token format');
+    }
+
+    const header = JSON.parse(atob(parts[0]));
+    const payload = JSON.parse(atob(parts[1]));
+
+    return { header, payload };
+  }
+
 function login_click(event) {
     event.preventDefault(); // 기본 동작 방지
     // 사용자를 42 인증 페이지로 리다이렉트
     // query parameter(?다음) 부분을 환경변수로 대체해야 한다.
-    window.location.href = "https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-0abe518907df9bbc76014c0d71310e3b0ed196727cec3f8d4e741103871a186d&redirect_uri=http%3A%2F%2F127.0.0.1%3A5500%2F&response_type=code";
+    window.location.href = "https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-13da844ab09c30f81a4aac6f7f77bd34bfa89523fd00822876ca6c9ab86ac14f&redirect_uri=http%3A%2F%2F127.0.0.1%3A5500&response_type=code";
 }
 document.addEventListener("DOMContentLoaded", async () => {
     const login_button = document.querySelector(".login");
@@ -26,20 +38,44 @@ document.addEventListener("DOMContentLoaded", async () => {
         // 데이터 준비
         const data = new URLSearchParams({
             grant_type: 'authorization_code',
-            client_id: 'u-s4t2ud-c8ed34a18722d3b06d337af57bfdb0d1508556b71f1df037d060f2d1a31e3314',
-            client_secret: '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',
+            client_id: 'u-s4t2ud-13da844ab09c30f81a4aac6f7f77bd34bfa89523fd00822876ca6c9ab86ac14f',
+            client_secret: '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',
             code: code,
-            redirect_uri: 'https://127.0.0.1:5500'
+            redirect_uri: 'http://127.0.0.1:5500'
         });
 
         // authorization code를 백엔드에 전송
-        await fetch("http://52.78.146.67/user-management/token", {
+        await fetch("https://localhost/user-management/token", {
             method: "POST",
-            body: {"code": code}
-        })
-        .then(response => {
-            console.log(response.json());
-        })
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ "code": code })
+          })
+          .then(response => response.json())
+          .then(data => {
+            console.log(data['jwt']);
+            // console.log(data.)
+            const decodedToken = decodeJWTManually(data['jwt']);
+            console.log(decodedToken);
+          })
+          .catch(error => {
+            console.error('Error:', error);
+          });
+        // await fetch("https://localhost/game/api/game/?page=1", {
+        //     method: "GET",
+        //     // body: {"code": code}
+        // })
+        // .then(response => {
+        //     console.log(response.json());
+        // })
+
+
+          // 사용 예
+        //   const decodedToken = decodeJWTManually('your.jwt.token');
+        //   console.log(decodedToken);
+
+        //   decodeJWTManually()
     }
 })
 
